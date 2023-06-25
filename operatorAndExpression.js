5819745778dca07f07a3cf99498550d052a7fa43
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2023-06-25 23:08:37 +0530

    Update operatorAndExpression.js

--- operatorAndExpression.js
+++ operatorAndExpression.js
@@ -1,9 +1,15 @@
 console.log("Opeator in Javascript")
 let a = 10;
 let b = 4;
+//Arithmetic operators
 console.log("a + b = ",a+b)
 console.log("a / b = ",a/b)
 console.log("a ** b = ",a**b )//power
 console.log("a % b = ",a%b)//remainder
 console.log("a++ = ",a++)
 console.log("a-- = ",a--)
+
+//Assingment operators
+a = 8;
+a+=5;
+console.log(a)
