fa3bea9ad80bda1e66e453f992d89625b54339b6
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2023-06-25 23:33:08 +0530

    conditional statements

--- conditional.js
+++ conditional.js
@@ -1,3 +1,10 @@
 const prompt=require("prompt-sync")({sigint:true});
 let a = prompt("Hey what's your age? ")
-console.log(typeof a)
+a = Number.parseInt(a)
+if(a>0){
+    console.log("this is an valid age")
+}
+else 
+{
+    console.log("this is an invalid age")
+}
