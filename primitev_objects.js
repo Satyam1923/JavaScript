ddffec2aa3a993f5a726798771a3ddb6f16f3600
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2023-06-25 22:29:18 +0530

    Create primitev_objects.js

---
+++ primitev_objects.js
@@ -0,0 +1,18 @@
+//pirimitve in js 
+let a = null;
+let b = 23;
+let c = true;
+let d = BigInt("567")+BigInt("3")
+let e = "harry"
+let f = Symbol("I am  a nice symbol")
+let g = undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+
+// objects in js
+const item = {
+    "harry": true,
+    "Shubh": false,
+    "Satyam": 67
+}
+console.log(item["Satyam"])
