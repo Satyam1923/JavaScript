5916d0e19eb2aa865671edb89d9635affb7071af
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2023-06-25 22:13:03 +0530

    Create vat_let_const.js

---
+++ vat_let_const.js
@@ -0,0 +1,14 @@
+console.log("JavaScript tutorial 3: var, let and const")
+var a = 45;
+let b = "harry";
+var c = null
+var d = undefined
+var e = 12.3
+const author = "satyam "
+console.log(author)
+{
+    let b = "this"
+    console.log(b)
+}
+console.log(d)
+console.log(b)
