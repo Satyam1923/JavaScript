5198df50d62b139aad21b87573e0ebbde37bdac5
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2023-06-25 22:38:47 +0530

    pratice set

---
+++ ch1Pratice.js
@@ -0,0 +1,9 @@
+let a = "Number"
+a = a+ 2
+console.log(a)
+console.log(typeof a)
+const b = {
+    c:"hello world"
+}
+b["c"] = "hello satyam"
+console.log(b["c"])
---
+++ dictionary.js
@@ -0,0 +1,6 @@
+let dic = {
+    "satyam":"person who always speak truth",
+    "harry": "teacher"
+}
+console.log(dic["satyam"])
+console.log(dic["harry"])
