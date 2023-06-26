6b95943e82e135bb3929e053e958c447e6e48b11
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2023-06-26 20:55:19 +0530

    Update praticeCh3.js

--- praticeCh3.js
+++ praticeCh3.js
@@ -15,3 +15,16 @@ for(let i=0;i<Object.keys(marks).length;i++){
 for(let key in marks){
     console.log("marks of "+key+" is "+marks[key])
 }
+
+//Problem 3
+let cn = 4;
+let i ;
+while(i!=cn){
+i= prompt("Enter a number ? ");}
+console.log("you have entered a correct number")
+
+//Problem 4
+const mean =(a,b,c,d)=>{
+    return (a+b+c+d)/4
+}
+console.log(mean(4,5,6,7))
