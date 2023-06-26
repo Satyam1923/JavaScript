2d187d7dfbe4bef21be638411d17dd2cccec4e67
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2023-06-26 16:16:28 +0530

    Update pratice.js

--- pratice.js
+++ pratice.js
@@ -1,9 +1,15 @@
 const prompt=require("prompt-sync")({sigint:true});
 let  a = prompt("Enter a number ");
 a = Number.parseInt(a);
+
+let b ={
+    c:"hello",
+    d:"world"
+}
 if(a%2==0){
     console.log("It is an even number "+ a);
 }
 else {
     console.log("It is an odd number "+a);
 }
+console.log(b["c"]+" "+b["d"]);
